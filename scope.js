0d56b5136ae80211287781b4cc89d8626f4f974b
tameemshahriar <you@example.com>
2021-02-05 22:36:31 +0600

    let & construct scope variable

---
+++ scope.js
@@ -0,0 +1,16 @@
+function sum(first, second) {
+    let result = first + second;
+    if(result > 9){
+        var mood = "happy";
+        mood = "fishy";
+        mood = "funky";
+        mood = "cranky";
+        //console.log(mood);
+    }
+    return result;
+}
+
+// const output = sum(3, 7);
+var day = "friday";
+console.log(day);
+// console.log(output);
