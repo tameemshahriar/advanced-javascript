5076de7776f96ffd5a77cc5407d48c45a5fa23bd
tameemshahriar <you@example.com>
2021-02-05 14:46:09 +0600

    double equals vs triple equals

---
+++ double-vs-triple-equal.js
@@ -0,0 +1,14 @@
+/*const first = 2;
+const second = "2";
+ if(first == second) { //double equal only checks value
+    console.log("condition is true");
+} */
+
+const first = 1; //1 is compared to true, 0 is to false
+const second = true;
+if(first === second) { //triple equal checks value and its type
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false");
+}
